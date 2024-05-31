3e2bb2e6e7393de5bd59651f57a4b09181153cc9
avanishgadhikar <133745149+avanishgadhikar@users.noreply.github.com>
2024-06-01 00:13:39 +0530

    Cleaning Repo #1

--- generated.s
+++
@@ -1,8 +0,0 @@
-.text
-.align 2           ; Ensure proper alignment (2^2 = 4 bytes)
-.global _start
-
-_start:
-  mov x8, #93      ; syscall number for exit
-  mov x0, #12      ; exit status
-  svc #0           ; make the system call
